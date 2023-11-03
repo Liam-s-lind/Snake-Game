e6c250b40fa1fd2d92f93b9cce64288cf161de18
Liam-s-lind <>
2023-11-03 23:44:19 +0000

    added game over function

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ var snakeBody = [];
 var foodX;
 var foodY;
 
+var gameOver = false;
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
@@ -31,6 +33,9 @@ window.onload = function() {
 }
 
 function update() {
+    if (gameOver) {
+        return;
+    }
     context.fillStyle="black"; //gives color to the bord
     context.fillRect(0, 0, board.height, board.width); // this make it start filling the color from point 0,0 of the board
 
@@ -56,6 +61,19 @@ function update() {
 
     for (let i =0; i < snakeBody.length; i++) {
         context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //Condition for game over
+    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+        gameOver = true;
+        alert("You Lose!")
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            alert("You Lose!")
+        }
     }
 }
 

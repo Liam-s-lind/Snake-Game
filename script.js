60782d2f30ecd13d9215305d92f5d7d36b6a7b4c
Liam-s-lind <>
2023-11-03 15:32:03 +0000

    added board layout and color filling for bord animation

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ var rows = 20;
 var cols = 20;
 var board;
 var context;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //this is used for the drawing on the board
+
+    update();
+}
+
+function update() {
+    context.fillStyle="black"; //gives color to the bord
+    context.fillRect(0, 0, board.height, board.width); // this make it start filling the color from point 0,0 of the board
+}

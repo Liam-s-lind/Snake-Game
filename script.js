7db73f71238f97857616cbbd878d4981496e5a6a
Liam-s-lind <>
2023-11-03 16:26:37 +0000

    added snake and food to the game

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ var cols = 20;
 var board;
 var context;
 
+//the snakes head
+var snakeY = blockSize * 5;
+var snakeX = blockSize * 5;
+
+//the snakes food
+var foodY = blockSize * 10;
+var foodX = blockSize * 10;
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
@@ -17,4 +25,10 @@ window.onload = function() {
 function update() {
     context.fillStyle="black"; //gives color to the bord
     context.fillRect(0, 0, board.height, board.width); // this make it start filling the color from point 0,0 of the board
+
+    context.fillStyle="blue"; // color for the snake
+    context.fillRect(snakeY, snakeX, blockSize, blockSize); // place for the snake
+
+    context.fillStyle="red"; //color for the food
+    context.fillRect(foodY, foodX, blockSize, blockSize); // place for the snakes food
 }
